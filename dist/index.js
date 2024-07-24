9251a7f640559ad68cc3300759d45393b2c7c17f
William <will27528@gmail.com>
2024-07-24 18:47:28 +0100

    build: update dist

--- dist/index.js
+++ dist/index.js
@@ -9222,7 +9222,7 @@ async function publish(apiUrl, apiKey, project, channel, version, fileGlobs) {
   }
 
   core.notice(`Publishing ${version.version} with ${files.length} files...`)
-  cors.notice(JSON.stringify(data))
+  core.notice(JSON.stringify(data))
   const data = getFormData(version, files)
   const response = await fetch(
     `${apiUrl}/v1/projects/${project}/channels/${channel}/versions/api`,
