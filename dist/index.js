485166e3995d80a5b7a6ce2289b7316b8a841f26
William <will27528@gmail.com>
2024-07-24 18:49:53 +0100

    fix: remove problematic stringify

--- dist/index.js
+++ dist/index.js
@@ -9224,8 +9224,6 @@ async function publish(apiUrl, apiKey, project, channel, version, fileGlobs) {
   core.notice(`Publishing ${version.version} with ${files.length} files...`)
 
   const data = getFormData(version, files)
-  core.notice(JSON.stringify(data))
-
   const response = await fetch(
     `${apiUrl}/v1/projects/${project}/channels/${channel}/versions/api`,
     {
